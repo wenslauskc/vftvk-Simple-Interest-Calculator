dea5a7c2b876313855eee361977ad0cdb674ab2a
Wenslaus Kachasu <wenslausnkachasu@gmail.com>
2021-09-01 20:38:28 +0200

    Modified functional simple interest calculator

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+//Calculate Results
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   p = document.getElementById("principal").value;
+   r = document.getElementById("rate").value;
+   y = document.getElementById("years").value;
+   result = document.getElementById("result").value;
+
+   var year = new Date().getFullYear();
+   
+   document.getElementById("result").innerHTML = 
+   "If you deposit" + " " + p + "<br/>" +
+   "At an interest of " + " " + r + "<br/>" +
+
+   "You will receive" + " " + (p*r*y/100);
+  
 }
         
